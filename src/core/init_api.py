df219d2be3cc284c6247b3fdcfb0aa3e8fea8442
GIVandez <gavkin.vanya@gmail.com>
2025-12-22 12:04:56 +0300

    independent source directories

--- src/core/init_api.py
+++ src/core/init_api.py
@@ -3,6 +3,9 @@ from fastapi.staticfiles import StaticFiles
 
 from routes import admin_router, frame_router, graphic_editor_router, page_router, project_router, user_router, auth_router
 
+import os
+from pathlib import Path
+
 # запуск сервера
 # uv run uvicorn main:app --reload
 # sudo systemctl start postgresql
@@ -18,15 +21,20 @@ from routes import admin_router, frame_router, graphic_editor_router, page_route
 
 app = FastAPI()
 
+# Получаем путь к текущему файлу
+current_dir = Path(__file__).parent.parent
+static_path = current_dir / "static" / "account"
+
+app.mount("/account", StaticFiles(directory=str(static_path)), name="account")
 
 # Монтируем папку стилей и скриптов
-app.mount("/account", StaticFiles(directory="D:/Code/five/plot_twister/src/static/account"), name="account")
-app.mount("/admin", StaticFiles(directory="D:/Code/five/plot_twister/src/static/admin"), name="admin")
-app.mount("/script", StaticFiles(directory="D:/Code/five/plot_twister/src/static/script"), name="script")
-app.mount("/storyboard", StaticFiles(directory="D:/Code/five/plot_twister/src/static/storyboard"), name="storyboard")
-app.mount("/auth", StaticFiles(directory="D:/Code/five/plot_twister/src/static/auth"), name="auth")
-app.mount("/project", StaticFiles(directory="D:/Code/five/plot_twister/src/static/project"), name="project")
-app.mount("/static", StaticFiles(directory="D:/Code/five/plot_twister/src/static"), name="static")
+app.mount("/account", StaticFiles(directory=f"{current_dir}/static/account"), name="account")
+app.mount("/admin", StaticFiles(directory=f"{current_dir}/static/admin"), name="admin")
+app.mount("/script", StaticFiles(directory=f"{current_dir}/static/script"), name="script")
+app.mount("/storyboard", StaticFiles(directory=f"{current_dir}/static/storyboard"), name="storyboard")
+app.mount("/auth", StaticFiles(directory=f"{current_dir}/static/auth"), name="auth")
+app.mount("/project", StaticFiles(directory=f"{current_dir}/static/project"), name="project")
+app.mount("/static", StaticFiles(directory=f"{current_dir}/static"), name="static")
 
 
 
